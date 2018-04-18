f3232926cd3a08a50210c483865a7402126e532b
SumitKhajanchi <khajanchi.sumit24@gmail.com>
2018-04-18 12:09:54 -0500

    EmployeeRecord app

---
+++ EmployeeRecord/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class EmployeerecordConfig(AppConfig):
+    name = 'EmployeeRecord'
---
+++ EmployeeRecord/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.utils import timezone
+
+class Post(models.Model):
+    employee_id = models.CharField(max_length=10)
+    name = models.CharField(max_length=50)
+    age = models.IntegerField()
+    gender = models.CharField(max_length=6)
+    experience = models.TextField()
+    joining_date = models.DateField()
+
+    def __str__(self):
+        return self.name
+    
+
+# Create your models here.
