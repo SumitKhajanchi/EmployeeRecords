8cfd2f233c6e6b33417b69681c82217c5b1b5539
SumitKhajanchi <khajanchi.sumit24@gmail.com>
2018-04-19 13:47:52 -0500

    EmployeeRecord app

--- EmployeeRecord/views.py
+++ EmployeeRecord/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
+from django.shortcuts import render, get_object_or_404
 def post_list(request):
     posts = Post.objects.order_by('joining_date')
     return render(request, 'EmployeeRecord/post_list.html', {'posts':posts})
-
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'EmployeeRecord/post_detail.html', {'post': post})
 # Create your views here.
